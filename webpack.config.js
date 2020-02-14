58829d1af2a629e3d35d572152878a4cf7835018
Hart Simha <hart.simha@suse.com>
2020-02-14 14:09:04 -0800

    Additional dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 module.exports = {
   entry: __dirname + '/app/index.js',
   module: {
-    loaders: [
+    rules: [
       {
-        test: \.js$/,
+        test: /\.js$/,
         exclude: /node_module/,
-        loader: 'babel-loader'
+        use: [{
+          loader: 'babel-loader',
+          options: {}
+        }]
       }
     ]
   },
   output: {
     filename: 'transformed.js',
     path: __dirname + '/build'
-  }
+  },
+  plugins: [HTMLWebpackPluginConfig]
 };
